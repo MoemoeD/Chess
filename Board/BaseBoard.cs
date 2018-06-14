dbd5755881b0dfb5a70b2252653fafda51a483ba
MoemoeD <37505963+MoemoeD@users.noreply.github.com>
2018-06-15 01:32:04 +0800

    签入棋类

---
+++ Board/BaseBoard.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board
+{
+    public abstract class BaseBoard
+    {
+        /// <summary>
+        /// 棋盘X轴
+        /// </summary>
+        protected int boardX { get; set; }
+
+        /// <summary>
+        /// 棋盘Y轴
+        /// </summary>
+        protected int boardY { get; set; }
+
+        /// <summary>
+        /// 间隙
+        /// </summary>
+        protected int gapPixel { get; set; }
+
+        /// <summary>
+        /// 棋盘基准坐标X
+        /// </summary>
+        protected int initialPixelX { get; set; }
+
+        /// <summary>
+        /// 棋盘基准坐标Y
+        /// </summary>
+        protected int initialPixelY { get; set; }
+
+        /// <summary>
+        /// 棋盘点位状态
+        /// </summary>
+        protected pieceType[,] state;
+
+        /// <summary>
+        /// 棋盘点位状态类型
+        /// </summary>
+        public enum pieceType
+        {
+            /// <summary>
+            /// 空
+            /// </summary>
+            Blank,
+
+            /// <summary>
+            /// 黑
+            /// </summary>
+            Black,
+
+            /// <summary>
+            /// 白
+            /// </summary>
+            White,
+        }
+    }
+}
---
+++ Board/GobangBoard.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Board
+{
+    public class GobangBoard : BaseBoard
+    {
+        private static GobangBoard _gobangBoard = new GobangBoard();
+
+        private GobangBoard()
+        {
+            this.boardX = 19;
+            this.boardY = 19;
+            this.gapPixel = 40;
+            this.initialPixelX = 100;
+            this.initialPixelY = 100;
+            this.state = new pieceType[this.boardX, this.boardY];
+        }
+
+        public static GobangBoard Instance()
+        {
+            return _gobangBoard;
+        }
+    }
+}
