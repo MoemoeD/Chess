a57ab1b733bfc5fc2dcec181d8eac5d82dcd6a5a
MoemoeD <37505963+MoemoeD@users.noreply.github.com>
2019-02-21 16:58:28 +0800

    Update BaseBoard.cs

--- Board/BaseBoard.cs
+++ Board/BaseBoard.cs
@@ -6,6 +6,8 @@ namespace Board
 {
     public abstract class BaseBoard
     {
+        #region -基础-
+
         /// <summary>
         /// 棋盘X轴
         /// </summary>
@@ -45,6 +47,90 @@ namespace Board
         /// 棋盘点位状态
         /// </summary>
         protected boardType[,] state { get; set; }
+
+        /// <summary>
+        /// 棋盘点位状态类型
+        /// </summary>
+        public enum boardType
+        {
+            /// <summary>
+            /// 空
+            /// </summary>
+            Blank,
+
+            /// <summary>
+            /// 黑
+            /// </summary>
+            Black,
+
+            /// <summary>
+            /// 白
+            /// </summary>
+            White,
+        }
+
+        /// <summary>
+        /// 获得棋盘点的具体坐标
+        /// </summary>
+        /// <param name="X"></param>
+        /// <param name="Y"></param>
+        /// <returns></returns>
+        public Point GetRealPointByBoardPoint(int X, int Y)
+        {
+            int pixelx = this.initialPixelx + X * this.gapPixel;
+            int pixely = this.initialPixely + Y * this.gapPixel;
+            return new Point(pixelx, pixely);
+        }
+
+        /// <summary>
+        /// 获取坐标所在范围的棋盘点
+        /// </summary>
+        /// <param name="pixelx"></param>
+        /// <param name="pixely"></param>
+        /// <param name="Point"></param>
+        /// <returns></returns>
+        public bool GetBoardRangeByRealPoint(int pixelx, int pixely, out Point Point)
+        {
+            //xy减去基准坐标加上半个间隙除以间隙得到XY
+            int X = (pixelx - this.initialPixelx + this.gapPixel / 2) / this.gapPixel;
+            int Y = (pixely - this.initialPixely + this.gapPixel / 2) / this.gapPixel;
+
+            //判断-0
+            int remainderx = (pixelx - this.initialPixelx + this.gapPixel / 2) % this.gapPixel;
+            int remaindery = (pixely - this.initialPixely + this.gapPixel / 2) % this.gapPixel;
+
+            Point = new Point(X, Y);
+
+            //超出边界
+            if (X >= this.boardX || Y >= this.boardY || remainderx < 0 || remaindery < 0)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 获取棋盘颜色
+        /// </summary>
+        /// <returns></returns>
+        public Color GetMainColor()
+        {
+            return this.mainColor;
+        }
+
+        /// <summary>
+        /// 获取棋盘背景颜色
+        /// </summary>
+        /// <returns></returns>
+        public Color GetBgColor()
+        {
+            return this.bgColor;
+        }
+
+        #endregion
+
+        #region -日志-
 
         /// <summary>
         /// 棋谱
@@ -108,27 +194,6 @@ namespace Board
         }
 
         /// <summary>
-        /// 棋盘点位状态类型
-        /// </summary>
-        public enum boardType
-        {
-            /// <summary>
-            /// 空
-            /// </summary>
-            Blank,
-
-            /// <summary>
-            /// 黑
-            /// </summary>
-            Black,
-
-            /// <summary>
-            /// 白
-            /// </summary>
-            White,
-        }
-
-        /// <summary>
         /// 行动状态类型
         /// </summary>
         protected enum actionType
@@ -149,6 +214,10 @@ namespace Board
             Victory,
         }
 
+        #endregion
+
+        #region -处理-
+
         /// <summary>
         /// 下棋
         /// </summary>
@@ -164,55 +233,6 @@ namespace Board
         /// <param name="form"></param>
         public abstract void DrawBoard(Form form);
 
-        /// <summary>
-        /// 获得棋盘点的具体坐标
-        /// </summary>
-        /// <param name="X"></param>
-        /// <param name="Y"></param>
-        /// <returns></returns>
-        public Point GetRealPointByBoardPoint(int X, int Y)
-        {
-            int pixelx = this.initialPixelx + X * this.gapPixel;
-            int pixely = this.initialPixely + Y * this.gapPixel;
-            return new Point(pixelx, pixely);
-        }
-
-        /// <summary>
-        /// 获取坐标所在范围的棋盘点
-        /// </summary>
-        /// <param name="pixelx"></param>
-        /// <param name="pixely"></param>
-        /// <param name="Point"></param>
-        /// <returns></returns>
-        public bool GetBoardRangeByRealPoint(int pixelx, int pixely, out Point Point)
-        {
-            //xy减去基准坐标加上半个间隙除以间隙得到XY
-            int X = (pixelx - this.initialPixelx + this.gapPixel / 2) / this.gapPixel;
-            int Y = (pixely - this.initialPixely + this.gapPixel / 2) / this.gapPixel;
-
-            //判断-0
-            int remainderx = (pixelx - this.initialPixelx + this.gapPixel / 2) % this.gapPixel;
-            int remaindery = (pixely - this.initialPixely + this.gapPixel / 2) % this.gapPixel;
-
-            Point = new Point(X, Y);
-
-            //超出边界
-            if (X >= this.boardX || Y >= this.boardY || remainderx < 0 || remaindery < 0)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        public Color GetMainColor()
-        {
-            return this.mainColor;
-        }
-
-        public Color GetBgColor()
-        {
-            return this.bgColor;
-        }
+        #endregion
     }
 }
